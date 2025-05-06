069706c96d6f8d8fb7a733a0130eb7868185fa9e
giantpreston <146558151+giantpreston@users.noreply.github.com>
2025-05-05 21:53:52 -0300

    Update constants.ts and add my name

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -589,6 +589,10 @@ export const Devs = /* #__PURE__*/ Object.freeze({
         name: "samsam",
         id: 836452332387565589n,
     },
+    GiantPreston: {
+        name: "GiantPreston",
+        id: 1054370257172709447n,
+    },
 } satisfies Record<string, Dev>);
 
 // iife so #__PURE__ works correctly
